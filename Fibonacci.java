d6199d5747490c036ae27cdcaed9c0f849700482
William Rodrigues de Sousa <rm78369@fiap.com.br>
2018-12-04 16:52:59 -0200

    Update Fibonacci.java
    Implementação do Scanner

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
 
 public class Fibonacci {
 
 	public static void main(String[] args) {
 		
+		int x;
+		
 		Fibonacci f = new Fibonacci();
 		
-		f.fib(0, 1, 1000);
+		System.out.println("Digite o número máximo que deseja para a sequência de Fibonacci");
+		Scanner sc = new Scanner(System.in);
+		x = sc.nextInt();
+		
+		f.fib(0, 1, x);
+		
+
 	}
 	
 	public int fib(int anterior, int atual, int max) {
